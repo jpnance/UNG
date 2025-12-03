966d902a2b38768f57c649d4fce424c14e67b048
Patrick Nance <jpnance@gmail.com>
2025-12-03 13:57:00 -0800

    Pointless now.

--- legacy/standings.mjs
+++ legacy/standings.mjs
@@ -1,8 +1,6 @@
 import superagent from 'superagent';
 import 'dotenv/config';
 
-const nytAssetKey = 'bb1c5f697293fe8bb92fa988e655b6389c4e1121';
-
 const disqualifiedPlayers = [];
 
 const teams = [
